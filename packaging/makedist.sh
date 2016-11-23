c1d0ad5343892376303903bc3f2bdd15906ea464
Clément OUDOT <clem.oudot@gmail.com>
2016-11-23 07:21:06 +0100

    Add cache and templates_c dirs in default archive

--- packaging/makedist.sh
+++ packaging/makedist.sh
@@ -18,11 +18,13 @@ rm -f $NAME*
 
 # Create dist dir
 mkdir -p $NAME-$VERSION
+mkdir -p $NAME-$VERSION/cache
 mkdir -p $NAME-$VERSION/conf
 mkdir -p $NAME-$VERSION/htdocs
 mkdir -p $NAME-$VERSION/lang
 mkdir -p $NAME-$VERSION/lib
 mkdir -p $NAME-$VERSION/templates
+mkdir -p $NAME-$VERSION/templates_c
 
 # Copy files
 cp    ../AUTHORS     $NAME-$VERSION
