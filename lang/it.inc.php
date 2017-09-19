5c0b1c9173c995c3449b82ae99b6d75be7a74fcf
Clément OUDOT <clem.oudot@gmail.com>
2017-09-19 18:20:19 +0200

    Import Italian language file (#4)

---
+++ lang/it.inc.php
@@ -0,0 +1,53 @@
+<?php
+
+#==============================================================================
+# Italian
+#==============================================================================
+
+$messages['advancedsearch'] = "Ricerca avanzata";
+$messages['displayentry'] = "Visualizza";
+$messages['dnrequired'] = "Necessario identificativo record";
+$messages['entriesfound'] = "records trovati";
+$messages['entryfound'] = "record trovato";
+$messages['false'] = "No";
+$messages['fromdate'] = "Da";
+$messages['gallery'] = "Galleria";
+$messages['label_businesscategory'] = "Categoria attivit&aacute;";
+$messages['label_carlicense'] = "Patente";
+$messages["label_created"] = "Creato";
+$messages['label_description'] = "Descrizione";
+$messages['label_displayname'] = "Nome visualizzato";
+$messages['label_employeenumber'] = "Matricola impiegato";
+$messages['label_employeetype'] = "Tipo impiegato";
+$messages['label_fax'] = "Fax";
+$messages['label_firstname'] = "Nome";
+$messages['label_fullname'] = "Nome completo";
+$messages['label_identifier'] = "Identificativo";
+$messages['label_l'] = "Localit&aacute;";
+$messages['label_lastname'] = "Cognome";
+$messages['label_mail'] = "Mail";
+$messages['label_manager'] = "Manager";
+$messages['label_mobile'] = "Cellulare";
+$messages["label_modified"] = "Modificato";
+$messages['label_organization'] = "Organizzazione";
+$messages['label_organizationalunit'] = "Unit&aacute; organizzativa";
+$messages['label_pager'] = "Cercapersone";
+$messages['label_phone'] = "Telefono";
+$messages['label_postaladdress'] = "Indirizzo";
+$messages['label_postalcode'] = "C.A.P.";
+$messages['label_secretary'] = "Segretario";
+$messages['label_state'] = "Stato";
+$messages['label_street'] = "Via";
+$messages['label_title'] = "Titolo";
+$messages['ldaperror'] = "Errore di comunicazione con LDAP";
+$messages['noentriesfound'] = "Nessun record trovato";
+$messages['notdefined'] = "Non definito";
+$messages['search'] = "Cerca";
+$messages['searchrequired'] = "Inserisci il termine di ricerca";
+$messages['sizelimit'] = "Limite dimensioni raggiunto, alcuni record non saranno visualizzati";
+$messages['title'] = "White Pages";
+$messages['todate'] = "A";
+$messages['true'] = "Si";
+$messages['welcome'] = "Benvenuto in LDAP Tool Box White Pages";
+
+?>
