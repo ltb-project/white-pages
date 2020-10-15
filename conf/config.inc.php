bf0fa0685d7ed58b0d5d8ff6bc2913962b0c5c15
Samuel MARTIN MORO <faust64@gmail.com>
2020-10-15 16:55:02 +0200

    feat(vcards): list group members
    dummy implementation / quick workaround
    can probably do better .... maybe later

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -140,7 +140,7 @@ $use_vcard = true;
 $vcard_file_extension = "vcf";
 $vcard_file_identifier = "identifier";
 $vcard_version = "4.0";
-$vcard_map = array('FN' => 'fullname', 'N' => 'fullname', 'EMAIL' => 'mail', 'CATEGORIES' => 'businesscategory', 'ORG' => 'organization', 'ROLE' => 'employeetype', 'TEL;TYPE=work,voice;VALUE=uri:tel' => 'phone', 'TEL;TYPE=cell,voice;VALUE=uri:tel' => 'mobile', 'UID' => 'identifier');
+$vcard_map = array('FN' => 'fullname', 'N' => 'fullname', 'EMAIL' => 'mail', 'CATEGORIES' => 'businesscategory', 'MEMBER' => 'member', 'ORG' => 'organization', 'ROLE' => 'employeetype', 'TEL;TYPE=work,voice;VALUE=uri:tel' => 'phone', 'TEL;TYPE=cell,voice;VALUE=uri:tel' => 'mobile', 'UID' => 'identifier');
 
 # Photo
 $use_gravatar = false;
