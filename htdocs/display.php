72324db04a33d1f2704aea1fa9539696414ee8e0
Samuel MARTIN MORO <faust64@gmail.com>
2020-10-31 09:53:50 +0100

    wip(display)
    member/memberof/uniquemember links shouldn't include a reference to
    current object type
    tries to detect object kind when display called with no type param,
    ldap_user_regexp not set, and user_base matches group_base

--- htdocs/display.php
+++ htdocs/display.php
@@ -39,12 +39,22 @@ if ($result === "") {
         } else {
             $type = "group";
         }
+    } else if ( strpos($ldap_user_base, $ldap_group_base) ) {
+	if ( strpos($ldap_group_base, $ldap_user_base) ) {
+	    $type = "guess";
+	} else if ( preg_match( '/'.$ldap_user_base.'$/i', $dn) ) {
+	    $type = "user";
+	} else if ( preg_match( '/'.$ldap_group_base.'$/i', $dn) ) {
+	    $type = "group";
+	} else {
+	    $type = "guess";
+	}
+    } else if ( preg_match( '/'.$ldap_user_base.'$/i', $dn) ) {
+	$type = "user";
+    } else if ( preg_match( '/'.$ldap_group_base.'$/i', $dn) ) {
+	$type = "group";
     } else {
-        if ( preg_match( '/'.$ldap_user_base.'$/i', $dn) ) {
-            $type = "user";
-        } else {
-            $type = "group";
-        }
+	$type = "guess";
     }
 
     if ($ldap) {
@@ -52,55 +62,89 @@ if ($result === "") {
         # Search attributes
         $attributes = array();
         $search_items = array();
-        if ( $type === "user" ) { $search_items = $display_items;  }
-        if ( $type === "group" ) { $search_items = $display_group_items;  }
-        foreach( $search_items as $item ) {
-            $attributes[] = $attributes_map[$item]['attribute'];
-        }
-        $attributes[] = $attributes_map[$display_title]['attribute'];
 
-        if ($use_vcard and isset($_GET["vcard"]) and $vcard_file_identifier) {
-            $attributes[] = $attributes_map[$vcard_file_identifier]['attribute'];
+        if ( $type === "guess" ) {
+	    $guess_filter = "(|".$ldap_user_filter.$ldap_group_filter.")";
+	    $guess_search = ldap_read($ldap, $dn, $guess_filter, [ "objectClass" ]);
+	    $errno = ldap_errno($ldap);
+
+	    if ( $errno ) {
+		$result = "ldaperror";
+		error_log("LDAP - Search error $errno  (".ldap_error($ldap).")");
+	    } else {
+		$guess_entry = ldap_get_entries($ldap, $guess_search);
+		foreach ($guess_entry[0] as $attr => $values) {
+		    if ( $values['count'] > 1 ) {
+			asort($values);
+		    }
+		    if ( isset($values['count']) ) {
+			unset($values['count']);
+		    }
+		    $guess_entry[0][$attr] = $values;
+		}
+		foreach ([ "groupOfName", "groupOfURLs", "groupOfUniqueNames" ] as $matchGroup) {
+		    if (strlen(array_search(strtolower($matchGroup),
+			    array_map('strtolower', $guess_entry[0]['objectClass']))) > 0) {
+			$type = "group";
+			break;
+		    }
+		}
+		if ( $type === "guess" ) {
+		    $type = "user";
+		}
+	    }
 	}
+	if ( $type != "guess" ) {
+	    if ( $type === "user" ) { $search_items = $display_items;  }
+	    if ( $type === "group" ) { $search_items = $display_group_items;  }
+	    foreach( $search_items as $item ) {
+		$attributes[] = $attributes_map[$item]['attribute'];
+	    }
+	    $attributes[] = $attributes_map[$display_title]['attribute'];
 
-        # Search entry
-        $ldap_filter = $ldap_user_filter;
-        if ($type === "group" ) {
-            $ldap_filter = $ldap_group_filter;
-        }
-        $search = ldap_read($ldap, $dn, $ldap_filter, $attributes);
+	    if ($use_vcard and isset($_GET["vcard"]) and $vcard_file_identifier) {
+		$attributes[] = $attributes_map[$vcard_file_identifier]['attribute'];
+	    }
 
-        $errno = ldap_errno($ldap);
+	    # Search entry
+	    $ldap_filter = $ldap_user_filter;
+	    if ($type === "group" ) {
+		$ldap_filter = $ldap_group_filter;
+	    }
+	    $search = ldap_read($ldap, $dn, $ldap_filter, $attributes);
 
-        if ( $errno ) {
-            $result = "ldaperror";
-            error_log("LDAP - Search error $errno  (".ldap_error($ldap).")");
-        } else {
-            $entry = ldap_get_entries($ldap, $search);
-        }
+	    $errno = ldap_errno($ldap);
 
-        # Sort attributes values
-        foreach ($entry[0] as $attr => $values) {
-            if ( $values['count'] > 1 ) {
-                asort($values);
-            }
-            if ( isset($values['count']) ) {
-                unset($values['count']);
-            }
-            $entry[0][$attr] = $values;
-        }
+	    if ( $errno ) {
+		$result = "ldaperror";
+		error_log("LDAP - Search error $errno  (".ldap_error($ldap).")");
+	    } else {
+		$entry = ldap_get_entries($ldap, $search);
+	    }
 
-	if ($use_vcard and isset($_GET["vcard"])) {
-            require_once("../lib/vcard.inc.php");
-            $vcard_file = $entry[0][$attributes_map[$vcard_file_identifier]['attribute']][0].".".$vcard_file_extension;
-            download_vcard_send_headers($vcard_file);
-            echo print_vcard($entry[0], $attributes_map, $vcard_map, $vcard_version);
-            die;
-        }
+	    # Sort attributes values
+	    foreach ($entry[0] as $attr => $values) {
+		if ( $values['count'] > 1 ) {
+		    asort($values);
+		}
+		if ( isset($values['count']) ) {
+		    unset($values['count']);
+		}
+		$entry[0][$attr] = $values;
+	    }
 
-	if ($display_edit_link) {
-		# Replace {dn} in URL
-		$edit_link = str_replace("{dn}", urlencode($dn), $display_edit_link);
+	    if ($use_vcard and isset($_GET["vcard"])) {
+		require_once("../lib/vcard.inc.php");
+		$vcard_file = $entry[0][$attributes_map[$vcard_file_identifier]['attribute']][0].".".$vcard_file_extension;
+		download_vcard_send_headers($vcard_file);
+		echo print_vcard($entry[0], $attributes_map, $vcard_map, $vcard_version);
+		die;
+	    }
+
+	    if ($display_edit_link) {
+		    # Replace {dn} in URL
+		    $edit_link = str_replace("{dn}", urlencode($dn), $display_edit_link);
+	    }
 	}
     }
 }
