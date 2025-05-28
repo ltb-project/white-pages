5af001c61a1b0ec2b52efc44925f996fd07c46ed
Clément OUDOT <clement.oudot@worteks.com>
2025-05-28 18:21:27 +0200

    Review: fix and clean directory page

--- htdocs/directory.php
+++ htdocs/directory.php
@@ -2,8 +2,6 @@
 /*
  * Display all entries in a table list
  */
-require_once("../conf/config.inc.php");
-require __DIR__ . '/../vendor/autoload.php';
 
 if (isset($_GET["type"])) {
     $type = $_GET["type"];
@@ -12,7 +10,7 @@ if (isset($_GET["type"])) {
 }
 
 if ( $type === "user" ) {
-    $ldap_search_base = $ldap_user_base;
+    $ldapInstance->ldap_user_base = $ldap_user_base;
     $ldap_search_filter = $ldap_user_filter;
     $result_items = $directory_items;
     $result_sortby = $directory_sortby;
@@ -20,35 +18,27 @@ if ( $type === "user" ) {
 }
 
 if ( $type === "group" ) {
-    $ldap_search_base = $ldap_group_base;
+    $ldapInstance->ldap_user_base = $ldap_group_base;
     $ldap_search_filter = $ldap_group_filter;
     $result_items = $directory_group_items;
     $result_sortby = $directory_group_sortby;
     $result_linkto = $directory_group_linkto;
 }
 
-if ($ldapInstance->connect()[0]) {
+# Connect to LDAP
+$ldap_connection = $ldapInstance->connect();
+
+$ldap = $ldap_connection[0];
+$result = $ldap_connection[1];
+
+if ($ldap) {
 
     # Search attributes
     foreach ($result_items as $item) $attributes[] = $attributes_map[$item]['attribute'];
 
     # Search for entries
-    [$ldap,$result,$nb_entries,$entries,$size_limit_reached] = $ldapInstance->search($ldap_search_filter, $attributes_list, $attributes_map, $search_result_title, $search_result_sortby, $result_items);
+    [$ldap, $result, $nb_entries, $entries, $size_limit_reached] = $ldapInstance->search($ldap_search_filter, $attributes, $attributes_map, $search_result_title, $result_sortby, $result_items);
 
-    if ( $result == 4) {
-        $size_limit_reached = true;
-    }
-    if ( $result != 0 and $result != 4 ) {
-        $result = "ldaperror";
-        error_log("LDAP - Search error $result  (".ldap_error($ldap).")");
-    } else {
-
-        if ($nb_entries === 0) {
-            $result = "noentriesfound";
-        } else {
-            unset($entries["count"]);
-        }
-    }
 }
 
 $smarty->assign("nb_entries", $nb_entries);
