107004797cbafe16835d13dd5d04769e6b3141cf
Clément OUDOT <clement@oodo.net>
2025-06-20 17:34:11 +0200

    Add API for dn_link edition

---
+++ htdocs/api/search_dn.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST["search"]) and $_POST["search"]) {
+
+    $filter_escape_chars = "";
+    if (!$search_use_substring_match) { $filter_escape_chars = "*"; }
+
+    $search_query = ldap_escape($_POST["search"], $filter_escape_chars, LDAP_ESCAPE_FILTER);
+
+    # Search filter
+    $ldap_filter = "(&".$ldap_user_filter."(|";
+    foreach ($quick_search_attributes as $attr) {
+        $ldap_filter .= "($attr=*$search_query*)";
+    }
+    $ldap_filter .= "))";
+
+    # Search attributes
+    $attributes = array('cn');
+
+    [$ldap,$result,$nb_entries,$entries,$size_limit_reached] = $ldapInstance->search($ldap_filter, $attributes, $attributes_map, $search_result_title, $search_result_sortby, $search_result_items, $ldap_scope);
+
+    if ($nb_entries) {
+        foreach($entries as $entry) {
+            $data[] = array( "dn" => $entry["dn"], "display" => $entry["cn"][0]);
+        }
+    }
+}
+?>
