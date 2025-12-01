1571c67092493a3700bf84a676acd9564a158761
Sébastien NOBILI <code@pipoprods.org>
2025-12-01 13:59:23 +0100

    feat: development environment

---
+++ packaging/docker/dev/config.inc.local.php
@@ -0,0 +1,12 @@
+<?php
+
+$ldap_url = 'ldap://ldap';
+$ldap_binddn = 'cn=admin,dc=example,dc=com';
+$ldap_bindpw = 'password';
+$ldap_user_base = $ldap_base;
+$use_updateinfos = true;
+$update_photo = true;
+$photo_local_ldap_attribute = 'photo';
+$require_auth = true;
+$auth_type = 'ldap';
+$display_myaccount_menu = true;
