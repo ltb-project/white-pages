cd0088e8319aec26b584f84c22151dfdd751021f
Arnault MICHEL <arnault.michel@gmail.com>
2026-02-20 23:04:39 +0100

    fix php syntax, tested with proxy basic auth

--- htdocs/geocode.php
+++ htdocs/geocode.php
@@ -30,32 +30,33 @@ $http_settings = array(
         'user_agent' => "LTB White-Pages address geocoding/1.0"
 );
 
-if  ($use_proxy) {
-    if ( isset($proxy_host)) and ($proxy_host != "") and (isset($proxy_port)) and ($proxy_port != "") {
+if  (isset($use_proxy) and $use_proxy) {
+    if ( isset($proxy_host) and ($proxy_host != "") and isset($proxy_port) and ($proxy_port != "")) {
         # proxy settings are valid
         $http_settings['proxy'] = "tcp://" . $proxy_host . ":" . $proxy_port;
+
+        if (isset($proxy_authentication_method)) {
+             if (isset($proxy_authentication_user) and isset($proxy_authentication_pass)){
+                if ($proxy_authentication_method === "basic") {
+                    $http_settings['header'] = "Proxy-Authorization: Basic " . base64_encode($proxy_authentication_user . ":" . $proxy_authentication_pass);
+                } else if ($proxy_authentication_method !== "none") {
+                    error_log("Proxy authentication method '" . $proxy_authentication_method . "' is not yet supported.");
+                }
+            } else {
+                error_log("Proxy authentication method is set to 'basic' but proxy authentication user and/or password is not set in config.inc.local.php.");
+            } 
+        }
+        
+        if ($proxy_request_fulluri) {
+            $http_settings['request_fulluri'] = true;
+        }
+        if ( isset($proxy_ssl_options) ) {
+            $http_settings['ssl'] = $proxy_ssl_options;
+        }
     } else {
         error_log("Proxy usage is enabled but proxy host and/or port is not set in config.inc.local.php.");
     }
     
-    if ($proxy_request_fulluri) {
-        $http_settings['request_fulluri'] = true;
-    }
-
-    if (isset($proxy_authentication_method)){
-        if ($proxy_authentication_method === "basic") {
-            if (isset($proxy_authentication_user) and isset($proxy_authentication_pass)) {
-                $http_settings['header'] = "Proxy-Authorization: Basic " . base64_encode($proxy_authentication_user . ":" . $proxy_authentication_pass);
-            } else {
-                error_log("Proxy authentication method is set to 'basic' but proxy authentication user and/or password is not set in config.inc.local.php.");
-            }
-        } else if ($proxy_authentication_method !== "none"){
-            error_log("Proxy authentication method '" . $proxy_authentication_method . "' is not yet supported.");
-        }
-    }
-    if ( isset($proxy_ssl_options) ) {
-        $http_settings['ssl'] = $proxy_ssl_options
-    }
 }
 
 $opts = array(
